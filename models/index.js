3a5c647a64ba4af550cade09471581226ed1c101
prkrgreenwell <prkrgreenwell@gmail.com>
2022-12-30 13:31:58 -0700

    handlebars and bootstrap work

--- models/index.js
+++ models/index.js
@@ -2,8 +2,9 @@
 
 const User = require("./User");
 const Post = require("./Post");
+const Comment = require("./Comment");
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
 });
@@ -12,4 +13,20 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
