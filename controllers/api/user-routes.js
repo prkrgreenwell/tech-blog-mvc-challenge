f9fa33b134db4e894cf341bba34a343bcc355ac4
prkrgreenwell <prkrgreenwell@gmail.com>
2023-01-10 22:19:53 -0700

    created layout for posts and comments

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,26 @@
+/** @format */
+
 const router = require("express").Router();
 const { User } = require("../../models");
+
+//CREATE new user
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,15 @@
+/** @format */
+
 const router = require("express").Router();
 const { User } = require("../models");
 
-//GET all Users
-router.get("/", async (req, res) => {
-  try {
-    const userData = await User.findAll();
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
+//Login Screen
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
   }
+  res.render("login");
 });
 
 module.exports = router;
