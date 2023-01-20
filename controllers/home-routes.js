5f26087c3a042b11db801b504d9f024af4eaccf1
PARKER GREENWELL <pgreenwell@alpine.local>
2023-01-20 12:10:41 -0700

    created dashboard page and create post modal, neither are functioning

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,4 +43,37 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/dashboard", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      // where: {
+      //   user_id: req.session.user_id,
+      // },
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+        {
+          model: Comment,
+          attributes: ["comment_text", "comment_time"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("dashboard", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
