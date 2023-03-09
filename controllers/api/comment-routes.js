0199dc0a2cf4d87a744f7deda2916a1b672d04f8
prkrgreenwell <prkrgreenwell@gmail.com>
2023-03-09 04:06:25 -0700

    comment functionality added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,5 +1,8 @@
+/** @format */
+
 const router = require("express").Router();
 const { Comment, Post, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -14,6 +17,21 @@ router.get("/", async (req, res) => {
 
     res.status(200).json(commentData);
   } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      comment_text: req.body.title,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
---
+++ public/js/create-comment.js
@@ -0,0 +1,77 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add("is-active");
+  }
+
+  function closeModal($el) {
+    $el.classList.remove("is-active");
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener("click", () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+
+    $close.addEventListener("click", () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener("keydown", (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) {
+      // Escape key
+      closeAllModals();
+    }
+  });
+});
+
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+  const comment_text = document.querySelector("#comment-content").value;
+
+  if (comment_text) {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        comment_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert("Failed to add comment");
+    }
+  }
+};
+
+document
+  .querySelector("#create-comment-form")
+  .addEventListener("submit", commentFormHandler);
